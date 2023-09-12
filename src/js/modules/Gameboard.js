cb8ca9a875154b90f2a8b69e056da9e497781797
Porkenvolf <89871231+porkenvolf@users.noreply.github.com>
2023-09-12 20:22:18 -0300

    Adds placeShip to the API, requires better testing

--- src/js/modules/Gameboard.js
+++ src/js/modules/Gameboard.js
@@ -1,6 +1,8 @@
 export default class Gameboard {
   #board = [];
 
+  #ships = [];
+
   constructor(x = 1, y = 1) {
     for (let i = 0; i < x; i++) {
       this.#board[i] = [];
@@ -10,7 +12,65 @@ export default class Gameboard {
     }
   }
 
+  placeShip(x, y, ship) {
+    const x1 = x;
+    const y1 = y;
+    const x2 = ship.getAxis()[0] * ship.getLength() + x1;
+    const y2 = ship.getAxis()[1] * ship.getLength() + y1;
+
+    if (this.#checkValid(x1, y1, x2, y2)) {
+      this.#ships.push({ ship, x, y });
+      this.#updateBoard();
+      return true;
+    }
+    return false;
+  }
+
   getBoard() {
     return this.#board;
+  }
+
+  #updateBoard() {
+    this.#ships.forEach((ship) => {
+      const x1 = ship.x;
+      const y1 = ship.y;
+      const x2 = ship.ship.getAxis()[0] * ship.ship.getLength() + x1;
+      const y2 = ship.ship.getAxis()[1] * ship.ship.getLength() + y1;
+
+      for (let i = x1; i <= x2; i++) {
+        for (let j = y1; j <= y2; j++) {
+          this.#board[i][j] = "S";
+        }
+      }
+    });
+  }
+
+  #checkValid(x1, y1, _x2 = null, _y2 = null) {
+    if (
+      x1 < 0 ||
+      y1 < 0 ||
+      x1 >= this.#board.length ||
+      y1 >= this.#board[0].length ||
+      _x2 < 0 ||
+      _y2 < 0 ||
+      _x2 >= this.#board.length ||
+      _y2 >= this.#board[0].length
+    ) {
+      return false;
+    }
+
+    let x2;
+    let y2;
+    if (!_x2 || !_y2) {
+      x2 = x1;
+      y2 = y1;
+    }
+    for (let i = x1; i <= x2; i++) {
+      for (let j = y1; j <= y2; j++) {
+        if (this.#board[i][j] !== " ") return false;
+      }
+    }
+
+    return true;
   }
 }
--- src/js/tests/Gameboard.test.js
+++ src/js/tests/Gameboard.test.js
@@ -1,10 +1,22 @@
 import Gameboard from "../modules/Gameboard";
+import Ship from "../modules/Ship";
 
 test("Board constructor works", () => {
   const x = 5;
   const y = 4;
   const testBoard = new Gameboard(x, y);
+  expect(testBoard.getBoard().length).toBe(5);
+  expect(testBoard.getBoard()[0].length).toBe(4);
+
   console.table(testBoard.getBoard());
-  expect(testBoard.getBoard().length).toBe(x);
-  expect(testBoard.getBoard()[0].length).toBe(y);
+});
+
+test("placeShip() works", () => {
+  const x = 5;
+  const y = 4;
+  const testBoard = new Gameboard(x, y);
+  const length = 3;
+  const testShip = new Ship(length, "x");
+  expect(testBoard.placeShip(0, 0, testShip)).toBe(true);
+  console.table(testBoard.getBoard());
 });
